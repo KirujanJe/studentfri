36bb85c56d41c834ac8c4b659990e0012e153441
Kirujan Jegatheeswaran <jegathki@sheridancollege.ca>
2023-01-27 12:55:11 -0500

    add part time student class

---
+++ src/student/partTimeStudent.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author kiruj
+ */
+// partTimeStudent is the child class
+// student class is the parent class, inheritance
+public class partTimeStudent extends Student { 
+    
+    private int numOfcourse;
+
+    public partTimeStudent(int numOfcourse, String student) {
+        super (studentId, studentName);
+        this.numOfcourse = numOfcourse;
+    }
+
+    public int getNumOfcourse() {
+        return numOfcourse;
+    }
+
+    public void setNumOfcourse(int numOfcourse) {
+        this.numOfcourse = numOfcourse;
+    }
+    
+}
