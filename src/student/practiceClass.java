e5a4dc3e4aa5ba85c1241e28de0fd0d5f4d9a0ac
kiruj <kiruj@KirujanPC.hitronhub.home>
2023-02-24 07:20:12 -0500

    This is an extra class I added to this package for practice from my home pc This is to practice: pull/push/commit

---
+++ src/student/practiceClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author kiruj
+ */
+public class practiceClass {
+    
+    public static void practiceClass(String [] args){
+        
+        System.out.println("Hello World");
+    
+    }
+    
+}
